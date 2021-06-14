276cf8cd382b2d7e7d2202c3ddf13872809e2e98
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-14 18:05:35 +0300

    init drag events

--- 02dragndrop/app.js
+++ 02dragndrop/app.js
@@ -1,15 +1,37 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart',dragstart )
 item.addEventListener('dragend' ,dragend )
 
+for (const placeholder of placeholders) {
+    console.log(placeholder)
+    placeholder.addEventListener('dragover')
+    placeholder.addEventListener('dragenter')
+    placeholder.addEventListener('dragleave')
+    placeholder.addEventListener('drop')
+}
+
 function dragstart(event) {
 console.log('drag start', event.target);
 event.target.classList.add('hold')
-event.target.classList.add('hide')
+setTimeout(() => event.target.classList.add('hide') , 0 )
 }
 
-function dragend() {
+function dragend(event) {
     console.log('drag end');
-    event.target.classList.remove('hold')
+    event.target.classList.remove('hold', 'hide')
+}
+
+function dragover(event) {
+
+}
+function dragenter(event) {
+
+}
+function dragleave(event) {
+
+}
+function dragdrop(event) {
+
 }
